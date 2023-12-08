336f4f9cdf2926b91a24dba13ca9dfd240ccf022
Alon Farchy <afarchy@virtualmaker.net>
2023-12-08 12:11:25 -0600

    Add ReadyPlayerMe avatar that can run around

---
+++ Assets/Needle/Components.codegen/Player.cs
@@ -0,0 +1,20 @@
+// NEEDLE_CODEGEN_START
+// auto generated code - do not edit directly
+
+#pragma warning disable
+
+namespace Needle.Typescript.GeneratedComponents
+{
+	public partial class Player : UnityEngine.MonoBehaviour
+	{
+		public UnityEngine.GameObject @camera;
+		public float @speed = 0.1f;
+		public float @rotateSpeed = 0.1f;
+		public UnityEngine.Vector3 @cameraOffset = new UnityEngine.Vector3(0f, 2f, -2);
+		public UnityEngine.Animator @animator;
+		public void start(){}
+		public void update(){}
+	}
+}
+
+// NEEDLE_CODEGEN_END
